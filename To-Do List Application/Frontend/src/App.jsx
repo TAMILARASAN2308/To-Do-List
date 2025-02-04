c1e8c22028aaa887296c0b7077f4cb73c5716673
TAMILARASAN M <140683736+TAMILARASAN2308@users.noreply.github.com>
2025-02-04 16:39:33 +0530

    Add files via upload

---
+++ To-Do List Application/Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import TodoList from './components/TodoList'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-[linear-gradient(45deg,#4C5CFF,#FF4FE0)] min-h-screen flex justify-center items-center'>
+    <TodoList/>
+    </div>
+    {/* <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1> */}
+    </>
+  )
+}
+
+export default App
