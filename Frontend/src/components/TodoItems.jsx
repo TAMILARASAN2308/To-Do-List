be94e8f51014bb20893c82f53a8da7a909e5b815
TAMILARASAN M <140683736+TAMILARASAN2308@users.noreply.github.com>
2025-02-04 23:26:02 +0530

    Frontend

---
+++ Frontend/src/components/TodoItems.jsx
@@ -0,0 +1,154 @@
+import { TiTick } from "react-icons/ti";
+import { HiX } from "react-icons/hi";
+import { PiNotePencilBold } from "react-icons/pi";
+import { MdDelete } from "react-icons/md";
+import { useEffect, useState } from "react";
+
+function TodoItems({ items, fetchItems }) {
+    const [isUpdateClicked, setIsUpdateClicked] = useState(null);
+    const [updatedTask, setUpdatedTask] = useState("");
+
+    useEffect(() => {
+        fetchItems(); 
+    }, []);
+
+    const handleChange = (e) => {
+        setUpdatedTask(e.target.value);
+    };
+
+    const toggleComplete = async (itemId) => {
+        try {
+            const response = await fetch(`https://to-do-list-3b2g.onrender.com/auth/iscomplete/${itemId}`, {
+                method: "PUT",
+            });
+
+            if (!response.ok) throw new Error("Failed to update item");
+
+            await fetchItems(); 
+        } catch (error) {
+            console.error("Error updating item:", error);
+        }
+    };
+
+    const deleteItem = async (itemId) => {
+        try {
+            const response = await fetch(`https://to-do-list-3b2g.onrender.com/auth/delete/${itemId}`, {
+                method: "DELETE",
+            });
+
+            if (!response.ok) throw new Error("Failed to delete item");
+
+            await fetchItems();
+        } catch (error) {
+            console.error("Error deleting item:", error);
+        }
+    };
+
+    const handleUpdate = async (itemId) => {
+        try {
+            const response = await fetch(`https://to-do-list-3b2g.onrender.com/update/${itemId}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ task: updatedTask }),
+            });
+
+            if (!response.ok) throw new Error("Failed to update task");
+
+            setIsUpdateClicked(null); 
+            setUpdatedTask(""); 
+            await fetchItems(); 
+        } catch (error) {
+            console.error("Error updating task:", error);
+        }
+    };
+
+    return (
+        <>
+            <section>
+                <div className="flex flex-col items-center cursor-pointer m-8 ">
+                    {items.map((item, index) => (
+                        <div key={item._id} className="flex flex-1 items-center justify-between w-full mb-4">
+                            {item.complete ? (
+                                <>
+                                    {isUpdateClicked === index ? (
+                                        <div className="flex items-center gap-2">
+                                        <input
+                                            className="text-black border-2 border-slate-400 rounded-md h-8 md:h-9 max-w-[170px] md:min-w-[220px] px-2 md:px-4"
+                                            type="text"
+                                            placeholder="Write your task"
+                                            value={updatedTask}
+                                            onChange={handleChange}
+                                        />
+                                        <button className="border bg-slate-300 rounded-md  p-1 px-1 md:px-2 ml-1 md:ml-4" onClick={() => handleUpdate(item._id)} >Update</button>
+                                        <button className="border bg-slate-300 rounded-md  p-1 px-1 md:px-2"onClick={()=>setIsUpdateClicked(!isUpdateClicked)}>Cancel</button>
+                                    </div>
+                                    ) : (
+                                        <>
+                                            <TiTick className="min-h-6 min-w-7" onClick={() => toggleComplete(item._id)} />
+                                            <div className="flex w-full">
+                                                <p className="text-slate-700 ml-4 line-through max-w-[150px] md:max-w-[250px] break-words">{item.task}</p>
+                                            </div>
+                                            <div className="flex gap-2">
+                                <PiNotePencilBold
+                                    className="h-6 w-7 cursor-pointer"
+                                    onClick={() => {
+                                        setIsUpdateClicked(isUpdateClicked === index ? null : index); 
+                                        setUpdatedTask(item.task);
+                                    }}
+                                />
+                                <MdDelete
+                                    className="h-6 w-7 cursor-pointer"
+                                    onClick={() => deleteItem(item._id)}
+                                />
+                            </div>
+                                        </>
+                                    )}
+                                </>
+                            ) : (
+                                <>
+                                    {isUpdateClicked === index ? (
+                                        <div className="flex items-center gap-2">
+                                            <input
+                                                className="text-black border-2 border-slate-400 rounded-md h-8 md:h-9 max-w-[170px] md:min-w-[220px] px-2 md:px-4"
+                                                type="text"
+                                                placeholder="Write your task"
+                                                value={updatedTask}
+                                                onChange={handleChange}
+                                            />
+                                            <button className="border bg-slate-300 rounded-md  p-1 px-1 md:px-2 ml-1 md:ml-4" onClick={() => handleUpdate(item._id)} >Update</button>
+                                            <button className="border bg-slate-300 rounded-md  p-1 px-1 md:px-2"onClick={()=>setIsUpdateClicked(!isUpdateClicked)}>Cancel</button>
+                                        </div>
+                                    ) : (
+                                        <>
+                                            <HiX className="min-h-6 min-w-7" onClick={() => toggleComplete(item._id)} />
+                                            <div className="flex w-full">
+                                                <p className="text-slate-700 ml-4 max-w-[180px] md:max-w-[250px] break-words">{item.task}</p>
+                                            </div>
+                                            <div className="flex gap-2">
+                                <PiNotePencilBold
+                                    className="h-6 w-7 cursor-pointer"
+                                    onClick={() => {
+                                        setIsUpdateClicked(isUpdateClicked === index ? null : index); 
+                                        setUpdatedTask(item.task);
+                                    }}
+                                />
+                                <MdDelete
+                                    className="h-6 w-7 cursor-pointer"
+                                    onClick={() => deleteItem(item._id)}
+                                />
+                            </div>
+                                        </>
+                                    )}
+                                </>
+                            )}
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default TodoItems;
---
+++ Frontend/src/components/TodoList.jsx
@@ -0,0 +1,77 @@
+import { LuListTodo } from "react-icons/lu";
+import TodoItems from "./TodoItems";
+import { useState } from "react";
+
+function TodoList() {
+    const [task, setTask] = useState({
+        name: "",
+        isComplete: false
+      });
+      const [items, setItems] = useState([]);
+
+      const handleChange = (e) => {
+        setTask({
+          ...task,
+          [e.target.name]: e.target.value
+        });
+      };
+      const fetchItems = async () => {
+        try {
+            const response = await fetch("https://to-do-list-3b2g.onrender.com/auth/items");
+            if (!response.ok) throw new Error("Failed to fetch items");
+            const data = await response.json();
+            setItems(data);
+        } catch (error) {
+            console.error("Error fetching items:", error);
+        }
+    };
+      
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!task.name.trim()) {
+          alert("Please enter a task.");
+          return; 
+        }
+        try {
+          const response = await fetch("https://to-do-list-3b2g.onrender.com/auth/task", { 
+            method: 'POST',
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              task: task.name,   
+              complete: task.isComplete
+            }) 
+          });
+      
+          if (response.ok) {
+            fetchItems();
+          } else {
+            alert("Failed to add task");
+          }
+        } catch (error) {
+          console.error("Error:", error);
+          alert("There was an error processing your request.");
+        }
+      };
+  return (
+    <>
+     <section className="font-poppins-font bg-white h-screen md:h-[600px] w-full md:w-1/3 md:rounded-xl p-7 flex flex-col">
+                <div className="flex md:mt-7 gap-2">
+                    <LuListTodo className="h-7 w-6 md:h-8 md:w-9"/>
+                    <h2 className="text-black text-xl md:text-2xl font-semibold">To-Do List</h2>
+                </div>
+                <form onSubmit={handleSubmit} className="flex justify-center mt-4 gap-2 mb-2">
+                    <input className="text-black border-2 border-slate-400 rounded-md w-1/2 px-4" type="text" placeholder="Add your task" name="name" onChange={handleChange}/>
+                    <button className="bg-[#0079FF] text-white w-32 h-12 rounded-xl cursor-pointer hover:bg-[#3AB0FF]" type="submit">Add</button>
+                </form>
+              
+                <div className="md:h-[400px] h-screen overflow-y-auto">
+                    <TodoItems items={items} fetchItems={fetchItems}/>
+                </div>
+      </section>
+    </> 
+  )
+}
+
+export default TodoList         
